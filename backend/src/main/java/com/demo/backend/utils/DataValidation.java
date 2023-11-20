499380eed64a25badcf84d8e5d54300fd580b846
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-11-20 16:28:54 +0200

    add static validation method

--- backend/src/main/java/com/demo/backend/utils/DataValidation.java
+++ backend/src/main/java/com/demo/backend/utils/DataValidation.java
@@ -4,19 +4,27 @@ import com.demo.backend.model.dto.TutorialDto;
 
 public class DataValidation {
 
-    public static void validateInputParamsOrElseThrowException(long id){
-        if(id <= 0){
-            throw new RuntimeException("id should be more then 0");
+    public static void validateInputParamsOrElseThrowException(long id) {
+        if (id <= 0) {
+            throw new RuntimeException("Id should be more then 0");
+        }
+    }
+
+    public static void validateInputParamsOrElseThrowException(String title) {
+        if (title == null
+                || title.isBlank()
+                || title.isEmpty()) {
+            throw new RuntimeException("Title should be string");
         }
     }
 
     public static void validateTutorialDtoRequest(TutorialDto tutorialDto) {
-        if(tutorialDto.getTitle() == null
+        if (tutorialDto.getTitle() == null
                 || tutorialDto.getTitle().isEmpty()
                 || tutorialDto.getTitle().isBlank()
                 || tutorialDto.getDescription() == null
                 || tutorialDto.getDescription().isEmpty()
-                || tutorialDto.getDescription().isBlank()){
+                || tutorialDto.getDescription().isBlank()) {
 
             throw new RuntimeException("Invalid Tutorial");
         }
