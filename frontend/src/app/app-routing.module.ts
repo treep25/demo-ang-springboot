7d4b2d77b2e2377def74529f19259a9001125bcc
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-11-22 12:15:48 +0200

    add error handling page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TutorialsListComponent} from './components/tutorials-list/tutorials-list.component';
+import {TutorialDetailsComponent} from './components/tutorial-details/tutorial-details.component';
+import {AddTutorialComponent} from './components/add-tutorial/add-tutorial.component';
+import {ModalComponent} from "./modal/modal.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
-  { path: 'tutorials', component: TutorialsListComponent },
-  { path: 'tutorials/:id', component: TutorialDetailsComponent },
-  { path: 'add', component: AddTutorialComponent }
+  {path: '', redirectTo: 'tutorials', pathMatch: 'full'},
+  {path: 'tutorials', component: TutorialsListComponent},
+  {path: 'tutorials/:id', component: TutorialDetailsComponent},
+  {path: 'add', component: AddTutorialComponent},
+  {path: 'error/fatal', component: ModalComponent}
 ];
 
 @NgModule({
