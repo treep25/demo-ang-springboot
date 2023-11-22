583df34091ec317d2e7b01e8d4138b88f61cf62c
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-11-22 10:00:06 +0200

    add function predicate map to perform request searching by two items

--- backend/src/main/java/com/demo/backend/service/TutorialService.java
+++ backend/src/main/java/com/demo/backend/service/TutorialService.java
@@ -1,12 +1,19 @@
 package com.demo.backend.service;
 
+import com.demo.backend.model.SearchingTutorialRequest;
 import com.demo.backend.model.Tutorial;
 import com.demo.backend.model.dto.TutorialDto;
 import com.demo.backend.repository.TutorialRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 import java.util.stream.StreamSupport;
 
 @Service
@@ -15,13 +22,22 @@ public class TutorialService {
 
     private final TutorialRepository tutorialRepository;
 
-    public List<Tutorial> getAllTutorials(){
+    private final Map<Predicate<SearchingTutorialRequest>, BiFunction<SearchingTutorialRequest, TutorialRepository, List<Tutorial>>> searchingMap = new HashMap<>() {{
+        put(request -> verifyRequestParamsNotEmptyAndBlank(request.getTitle()), (request, repository) -> repository.findByTitle(request.getTitle()));
+        put(request -> verifyRequestParamsNotEmptyAndBlank(request.getDescription()), (request, repository) -> repository.findByDescription(request.getDescription()));
+    }};
+
+    private boolean verifyRequestParamsNotEmptyAndBlank(String request) {
+        return request != null && !request.isEmpty() && !request.isBlank();
+    }
+
+    public List<Tutorial> getAllTutorials() {
         return StreamSupport.stream(
-                tutorialRepository.findAll().spliterator(),false
+                tutorialRepository.findAll().spliterator(), false
         ).toList();
     }
 
-    public Tutorial getTutorialById(long id){
+    public Tutorial getTutorialById(long id) {
         return tutorialRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
@@ -45,8 +61,14 @@ public class TutorialService {
         tutorialRepository.deleteAll();
     }
 
-    public List<Tutorial> findTutorialByTitle(String title) {
-        return tutorialRepository.findByTitle(title);
+    public List<Tutorial> findTutorialByDifferentParams(SearchingTutorialRequest searchingTutorialRequest) {
+        AtomicReference<List<Tutorial>> response = new AtomicReference<>(new ArrayList<>());
+        searchingMap.forEach((searchingTutorialRequestPredicate, repositoryAction) -> {
+            if (searchingTutorialRequestPredicate.test(searchingTutorialRequest)) {
+                response.set(repositoryAction.apply(searchingTutorialRequest, tutorialRepository));
+            }
+        });
+        return response.get().isEmpty() ? null : response.get();
     }
 
     public Tutorial updateTutorialById(long id, TutorialDto tutorialDto) {
