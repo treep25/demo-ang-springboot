2daded8ff0f1f9b6e73aa45667d96472ee30f2a1
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-11-23 08:56:38 +0200

    fixed status of tutorial

--- frontend/src/app/components/add-tutorial/add-tutorial.component.ts
+++ frontend/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Tutorial} from 'src/app/models/tutorial.model';
+import {Status, Tutorial} from 'src/app/models/tutorial.model';
 import {TutorialService} from 'src/app/services/tutorial.service';
 
 @Component({
@@ -12,7 +12,7 @@ export class AddTutorialComponent {
   tutorial: Tutorial = {
     title: '',
     description: '',
-    published: false
+    status: Status.PENDING
   };
   submitted = false;
 
@@ -47,7 +47,7 @@ export class AddTutorialComponent {
     this.tutorial = {
       title: '',
       description: '',
-      published: false
+      status: Status.PENDING
     };
   }
 }
