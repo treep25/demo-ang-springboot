46abeffa796f64419b8237f485d2049199f2e0ff
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-11-30 15:24:41 +0200

    add new search endpoints

--- backend/src/main/java/com/demo/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/demo/backend/user/repository/UserRepository.java
@@ -4,9 +4,14 @@ import com.demo.backend.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<User> findByFirstName(String firstName);
+
+    List<User> findByLastName(String lastName);
 }
