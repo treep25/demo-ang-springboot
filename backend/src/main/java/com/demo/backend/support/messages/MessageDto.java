ba5ee5d1d2ab92fba1d93f3a32677394fa145d5d
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-13 15:09:42 +0200

    add recipients field in MessageDto

--- backend/src/main/java/com/demo/backend/support/messages/MessageDto.java
+++ backend/src/main/java/com/demo/backend/support/messages/MessageDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +14,5 @@ import lombok.NoArgsConstructor;
 public class MessageDto {
     private String content;
     private String recipient;
-
+    private List<String> recipients;
 }
