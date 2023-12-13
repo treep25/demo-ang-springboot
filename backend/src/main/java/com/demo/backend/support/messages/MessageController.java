d3263263a667808983039e4d5202100a195eab89
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-13 15:08:57 +0200

    add a couple endpoints with group messages

--- backend/src/main/java/com/demo/backend/support/messages/MessageController.java
+++ backend/src/main/java/com/demo/backend/support/messages/MessageController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/v1/texting")
@@ -13,19 +15,37 @@ import org.springframework.web.bind.annotation.*;
 public class MessageController {
 
     private final MessageService messageService;
+    private final GroupMessageService groupMessageService;
 
     @PostMapping()
-//    @PreAuthorize("@permissionCheck.hasPermission(#user)")
     public ResponseEntity<?> text(@AuthenticationPrincipal User user, @RequestBody MessageDto messageDto) {
 
         messageService.text(messageDto, user);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("find/conversation/{recipientEmail}")
-//    @PreAuthorize("@permissionCheck.hasPermission(#user)")
+    @GetMapping("/find/conversation/{recipientEmail}")
     public ResponseEntity<?> findConversation(@AuthenticationPrincipal User user, @PathVariable String recipientEmail) {
 
         return ResponseEntity.ok(messageService.findConversation(user, recipientEmail));
     }
+
+    @GetMapping("/group/messages")
+    public ResponseEntity<?> getAllGroups(@AuthenticationPrincipal User user) {
+
+        return ResponseEntity.ok(groupMessageService.getAllGroups(user));
+    }
+
+    @PostMapping("/find/conversation/groups")
+    public ResponseEntity<?> findConversationOfGroups(@AuthenticationPrincipal User user, @RequestBody List<String> recipients) {
+        return ResponseEntity.ok(groupMessageService.findConversation(user, recipients));
+    }
+
+    @PostMapping("/group/message")
+    public ResponseEntity<?> textGroup(@AuthenticationPrincipal User user, @RequestBody MessageDto messageDto) {
+        groupMessageService.textToGroup(messageDto, user);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
