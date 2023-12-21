03afdcd769193ad1f667b301c7b1984efe887528
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-21 08:26:16 +0200

    changes to new type of security

--- backend/src/main/java/com/demo/backend/security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/demo/backend/security/config/SecurityConfiguration.java
@@ -3,18 +3,28 @@ package com.demo.backend.security.config;
 
 import com.demo.backend.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -24,39 +34,75 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationEntryPoint userAuthEntryPoint;
 
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:8081");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    public WebMvcConfigurer cors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8081")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(
+            @Qualifier("googleOAuth2") ClientRegistration googleOAuth2) {
+        return new InMemoryClientRegistrationRepository(googleOAuth2);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.security.oauth2.client.registration.google")
+    public ClientRegistration googleOAuth2() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("890569205307-aej1pgl3a88v83frp5dqp6aegikci2jl.apps.googleusercontent.com")
+                .clientSecret("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")
+                .scope("email", "profile")
+                .redirectUri("http://localhost:8080/login/oauth2/code/google")
+                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("email")
+                .clientName("Google")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/swagger-ui.html", "/v2/api-docs", "/webjars/**", "/swagger-resources/**", "/api-docs/**", "/v3/api-docs/**", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
-                .permitAll()
-                .requestMatchers("")
-                .permitAll()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthEntryPoint))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling();
+                .authorizeHttpRequests
+                        (authorize ->
+                                authorize
+                                        .requestMatchers(
+                                                "/swagger-ui.html",
+                                                "/v2/api-docs",
+                                                "/webjars/**",
+                                                "/swagger-resources/**",
+                                                "/api-docs/**",
+                                                "/v3/api-docs/**",
+                                                "/configuration/ui",
+                                                "/swagger-resources/**",
+                                                "/configuration/security",
+                                                "/swagger-ui.html",
+                                                "/webjars/**")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .permitAll())
+                .oauth2Login(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
