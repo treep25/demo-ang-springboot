864dbe96dcdb9847608326fdea73e4e9b34b7b39
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-21 15:26:01 +0200

    add oauth2

---
+++ backend/src/main/java/com/demo/backend/config/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.demo.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class WebClientConfig {
+
+    @Value("${stripe.security.oauth2.resourceserver.opaque-token.introspection-uri}")
+    private String introspectUri = "https://www.googleapis.com/";
+
+    @Bean
+    public WebClient userInfoClient() {
+        return WebClient.builder().baseUrl(introspectUri).build();
+    }
+}
