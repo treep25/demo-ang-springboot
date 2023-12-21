1f530b3a13be9af8a5b34797d5d9e28f686b3f3f
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-21 15:27:25 +0200

    replace old methods with lambdas

--- backend/src/main/java/com/demo/backend/security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/demo/backend/security/config/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package com.demo.backend.security.config;
 
 
+import com.demo.backend.config.GoogleOpaqueTokenIntrospector;
 import com.demo.backend.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,14 +13,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -35,6 +30,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationEntryPoint userAuthEntryPoint;
+    private final WebClient userInfoClient;
 
     @Bean
     public WebMvcConfigurer cors() {
@@ -50,36 +46,15 @@ public class SecurityConfiguration {
         };
     }
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(
-            @Qualifier("googleOAuth2") ClientRegistration googleOAuth2) {
-        return new InMemoryClientRegistrationRepository(googleOAuth2);
-    }
-
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.security.oauth2.client.registration.google")
-    public ClientRegistration googleOAuth2() {
-        return ClientRegistration.withRegistrationId("google")
-                .clientId("890569205307-aej1pgl3a88v83frp5dqp6aegikci2jl.apps.googleusercontent.com")
-                .clientSecret("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")
-                .scope("email", "profile")
-                .redirectUri("http://localhost:8080/login/oauth2/code/google")
-                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
-                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
-                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-                .userNameAttributeName("email")
-                .clientName("Google")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .build();
+    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return new GoogleOpaqueTokenIntrospector(userInfoClient);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthEntryPoint))
                 .authenticationProvider(authenticationProvider)
                 .authorizeHttpRequests
@@ -100,7 +75,7 @@ public class SecurityConfiguration {
                                         .permitAll()
                                         .anyRequest()
                                         .permitAll())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(c -> c.opaqueToken(Customizer.withDefaults()))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
