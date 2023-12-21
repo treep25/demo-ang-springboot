9f372b7eee25a368a586888385b611b7df0ddd94
Bohdan Hryhorenko <bogdan.grigorenko@faceit.com.ua>
2023-12-21 15:25:43 +0200

    Csrf token controller

--- backend/src/main/java/com/demo/backend/config/csrf/CsrfController.java
+++ backend/src/main/java/com/demo/backend/config/csrf/CsrfController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = {"X-CSRF-TOKEN"})
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/v1/token")
 @RequiredArgsConstructor
